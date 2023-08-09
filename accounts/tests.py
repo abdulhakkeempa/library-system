e464112e2d8f8b180af8c8527bb6daa3c2d1858e
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-08-09 21:47:38 +0530

    utils: added script file to generate random .env

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from accounts.models import Member, User
 # Create your tests here.
+
+class LibrarianTest(TestCase):
+    def setUp(self) -> None:
+        user = User.objects.create_user(email="test@library.com",full_name="Test", password="Test@123")
+        self.member = Member.objects.create(account=user)
+
+    def check_member_debt_balance(self):
+        self.member
+        self.assertEqual(self.member.isDebtOutstanding(), False)
