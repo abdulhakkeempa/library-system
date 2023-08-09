7fb3e06d7080e3422331b04dd463ce00fe793865
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-08-09 14:19:14 +0530

    Feature: Updated the default Auth User Model of Django

--- library/settings.py
+++ library/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#updating the custom user model
+AUTH_USER_MODEL = "accounts.User"
