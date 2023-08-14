9fc08b5f67b711adcd680156d6901ba7202f9991
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-08-14 15:27:48 +0530

    fix: url name fix

--- books/urls.py
+++ books/urls.py
@@ -5,8 +5,8 @@ app_name = 'books'
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='all'),
-    path('films/<int:pk>/detail', views.BookDetailView.as_view(), name='book_detail'),
-    path('films/create/', views.BookCreateView.as_view(), name='book_create'),
-    path('films/<int:pk>/update/', views.BookUpdateView.as_view(), name='book_update'),
-    path('films/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
+    path('<int:pk>/detail', views.BookDetailView.as_view(), name='book_detail'),
+    path('create/', views.BookCreateView.as_view(), name='book_create'),
+    path('<int:pk>/update/', views.BookUpdateView.as_view(), name='book_update'),
+    path('<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
 ]
